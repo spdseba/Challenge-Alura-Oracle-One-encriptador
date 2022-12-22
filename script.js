a97f379cc3709dff5b4425af3311c5252ddce775
Sebastian Perez <spdseba@gmail.com>
2022-12-22 10:04:58 -0300

    correccion de estilos y mensajes de error y exito

--- script.js
+++ script.js
@@ -1,32 +1,73 @@
 let botonEncriptar = document.querySelector("#boton-encriptar");
 let botonDesencriptar = document.querySelector("#boton-desencriptar");
 let resultado = document.querySelector(".resultado");
+let resultadoImagen = document.querySelector(".resultado img");
+let resultadoTitulo = document.querySelector(".resultado h2");
+let resultadoTexto = document.querySelector(".resultado p");
+let botonCopiar = document.querySelector("#boton-copiar");
+let alerta = document.querySelector(".alerta");
+console.log(resultado);
 
 botonEncriptar.addEventListener('click', encriptarTexto);
 botonDesencriptar.addEventListener('click', desencriptarTexto);
+botonCopiar.addEventListener('click', copiarTexto);
 
 
 function encriptarTexto(e){
     let texto = document.querySelector("#texto").value;
-    let textoEncriptado = cifrar(texto);
-    resultado.innerHTML = "";
-    let parrafo = document.createElement("p");
-    parrafo.textContent = textoEncriptado;
-    resultado.appendChild(parrafo);
+    if(texto == ""){
+        mensajeTextoVacio();
+    }else{
+        let textoEncriptado = cifrar(texto);
+        resultadoImagen.classList = "ocultar";
+        resultadoTitulo.classList =  "ocultar";
+        let parrafo = resultadoTexto;
+        parrafo.classList = "encriptado";
+        resultadoTexto.textContent = textoEncriptado;
+        botonCopiar.classList =  "mostrar";
+        botonDesencriptar.removeAttribute("disabled");
+    }
+
 }
 function desencriptarTexto(e){
     let texto = document.querySelector("#texto").value;
-    let textoDesencriptado = descifrar(texto);
-    resultado.innerHTML = "";
-    let parrafo = document.createElement("p");
-    parrafo.textContent = textoDesencriptado;
-    resultado.appendChild(parrafo);
+    if(texto == ""){
+        mensajeTextoVacio();
+    }else{
+        let textoDesencriptado = descifrar(texto);
+        resultadoImagen.classList = "ocultar";
+        resultadoTitulo.classList =  "ocultar";
+        let parrafo = resultadoTexto;
+        parrafo.textContent = textoDesencriptado;
+    }
+
 }
-
-
+function copiarTexto(e){
+    alerta.classList.add("exito");
+    navigator.clipboard.writeText(resultadoTexto.textContent)
+    .then(() => {
+        console.log('Texto copiado en el Portapapeles');
+    })
+    .catch(err => {
+        console.error('Error al copiar el texto: ', err);
+    });
+    alerta.textContent = "Texto copiado correctamente";
+    setTimeout(() => {
+        alerta.classList.remove("exito");
+    }, 2000);
+    return resultadoTexto.textContent;
+}
+function mensajeTextoVacio(){
+    alerta.classList.remove("exito");
+    alerta.classList.add("error");
+    alerta.textContent = "Por favor ingrese un texto valido";
+    setTimeout(() => {
+        alerta.classList.remove("error");
+    }, 2000);
+}
 function cifrar(texto){
     /*
-        Funcion que cifra un texto que recibe por parametros y retorna su valor
+        Función que cifra un texto que recibe por parámetros y retorna su valor
     */
     /*
     let textoEncriptado = texto.replace(/e/gi, "esser");
@@ -37,7 +78,7 @@ function cifrar(texto){
 
     */
     /* 
-    Reemplazo por medio del metodo replace una letra obtenida en la expresion regular por un conjunto de caracteres
+    Reemplazo por medio del método replace una letra obtenida en la expresión regular, por un conjunto de caracteres
     */
     let textoEncriptado = texto.replace(/e/gi, "esser").replace(/i/gi, "itrs").replace(/a/gi, "aplt").replace(/o/gi, "orwqa").replace(/u/gi, "uinxb");
     return textoEncriptado;
